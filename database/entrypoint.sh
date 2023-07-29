718b085890aaf06f9d5ecc45776fc7f1ac4a62b2
Vinícius Lucas <viniciuslucas1564@gmail.com>
2023-07-29 16:29:28 -0300

    Added replication config

--- database/entrypoint.sh
+++ database/entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 export PGPASSWORD='admin'
+
 psql -U 'admin' <<- EOSQL
     CREATE DATABASE dev;
+    CREATE USER rep_admin REPLICATION LOGIN ENCRYPTED PASSWORD 'rep_admin';
 EOSQL
+
 psql -U 'admin' -d 'dev' <<- EOSQL
 	CREATE TABLE "product_orders" (
         id UUID PRIMARY KEY,
@@ -12,3 +15,12 @@ psql -U 'admin' -d 'dev' <<- EOSQL
         status VARCHAR(9) NOT NULL
     );
 EOSQL
+
+psql -U 'admin' -d 'dev' -c "SELECT pg_create_physical_replication_slot('rep_slot');"
+
+pg_ctl stop -D /var/lib/postgresql/data -m fast
+
+cp /docker-entrypoint-initdb.d/pg_hba.conf /var/lib/postgresql/data/
+cp /docker-entrypoint-initdb.d/postgresql.conf /var/lib/postgresql/data/
+
+/usr/local/bin/docker-entrypoint.sh postgres
